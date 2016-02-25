c6367751c1e4152bd0e5e3503f0a5593fdcd308b
yanfengzhu <yzhu2@mail.sfsu.edu>
2016-02-25 22:58:05 +0000

    haha

---
+++ app.js
@@ -0,0 +1,59 @@
+var app = angular.module('flapperNews', ['ui.router'])
+ 
+   app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+    
+
+  $urlRouterProvider.otherwise('home');
+  
+}]);
+
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+
+app.controller('MainCtrl', [
+'$scope','posts',
+function($scope, posts){
+
+  $scope.addPost = function() { 
+    
+$scope.posts = posts.posts;
+  $scope.posts=[];
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
+  $scope.title = '';
+  $scope.link = '';
+};
+
+$scope.incrementUpvotes = function(post) {
+  post.upvotes += 1;
+};
+
+
+
+
+
+
+
+
+}]);
+
